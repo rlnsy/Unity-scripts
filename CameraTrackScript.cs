4952d17ca5e71efc0f4134c24745e86803c52d1a
rowlindsay <rowlindsay@gmail.com>
2018-06-09 22:58:19 -0700

    scripts

---
+++ CameraTrackScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraTrackScript : MonoBehaviour {
+
+    public Transform target;
+
+	// Use this for initialization
+	void Start () {	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+        float newX = target.transform.position.x;
+        float newY = target.transform.position.y + 1.5f;
+        float newZ = target.transform.position.z -2f;
+        transform.position = new Vector3(newX, newY, newZ);
+
+        //transform.rotation = target.transform.rotation;
+        float angle = target.transform.eulerAngles.y;
+
+        print(angle);
+
+        //transform.rotation = Quaternion.AngleAxis(-1 * angle, target.up);
+
+        //transform.LookAt(target);
+
+        transform.RotateAround(target.position, target.up, angle);
+        transform.LookAt(target);
+        transform.RotateAround(transform.position, transform.right, -25f);
+	}
+}
---
+++ MoveScript.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveScript : MonoBehaviour {
+
+    float moveSpeed = 5f;
+    float rotationAngle;
+
+	// Use this for initialization
+	void Start () {
+        rotationAngle = 0f;	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+        if(Input.GetKeyDown(KeyCode.R))
+        {
+            GetComponent<Renderer>().material.color = Color.red;
+        }
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            GetComponent<Renderer>().material.color = Color.green;
+        }
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            GetComponent<Renderer>().material.color = Color.blue;
+        }
+
+
+        // move
+
+        if (Input.GetKey(KeyCode.UpArrow)) {
+            transform.Translate(Vector3.forward * Time.deltaTime * moveSpeed);    
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.Translate(Vector3.back * Time.deltaTime * moveSpeed);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            //transform.Translate(Vector3.left * Time.deltaTime * moveSpeed);
+            Turn(true);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            //transform.Translate(Vector3.right * Time.deltaTime * moveSpeed);
+            Turn(false);
+        }
+
+	}
+
+    void Turn(bool leftTurn) {
+        rotationAngle += 5f * (leftTurn ? -1 : 1);
+        Quaternion rotation = Quaternion.AngleAxis(rotationAngle, transform.up);
+        transform.localRotation = rotation;
+    }
+
+    float getRotation() {
+        return rotationAngle;
+    }
+}
